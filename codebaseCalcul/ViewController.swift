45de13fed4ea59dbc81ac6903157d7a1fe5dc634
Lime <qorwlgml2003@naver.com>
2024-06-21 13:14:15 +0900

    [20240621]Lv1 완료

--- codebaseCalcul/ViewController.swift
+++ codebaseCalcul/ViewController.swift
@@ -11,11 +11,23 @@ class ViewController: UIViewController {
     private var label = UILabel()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configurateUI()
     }
     
     private func configurateUI(){
+        label.text = "12345"
+        label.textColor = .white
+        label.textAlignment = .right
+        label.font = UIFont.boldSystemFont(ofSize: 60)
         
+        [label].forEach{
+            view.addSubview($0)
+        }
+        label.snp.makeConstraints{
+            $0.trailing.leading.equalToSuperview().inset(30)
+            $0.top.equalTo(view.snp.top).offset(200)
+            $0.height.equalTo(100)
+        }
     }
 
 
